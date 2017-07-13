2f16e68c1f0d071809c4075e26045f5ad98fa32d
Zszhen <943001688@qq.com>
2017-07-13 17:14:50 +0800

    modify

--- src/main/java/DAO/TutorDAO.java
+++ src/main/java/DAO/TutorDAO.java
@@ -12,7 +12,7 @@ public class TutorDAO {
 	public static final String DRIVER="org.gjt.mm.mysql.Driver";
 	public static final String DBURL="jdbc:mysql://localhost:3306/jiajiaowang?useUnicode=true&characterEncoding=utf-8";
 	public static final String DBUSER="root";
-	public static final String DBPASS="1125";
+	public static final String DBPASS="zsz";
     
 	private Connection conn=null;
 	private PreparedStatement pStat=null;
