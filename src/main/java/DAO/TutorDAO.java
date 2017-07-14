4d9339f4b74f1fccdc4c377b006dc6d5a473f63b
huqiuting <huqiuting1@163.com>
2017-07-14 09:19:21 +0800

    psd

--- src/main/java/DAO/TutorDAO.java
+++ src/main/java/DAO/TutorDAO.java
@@ -11,9 +11,9 @@ import beans.Tutor;
 public class TutorDAO {
 	public static final String DRIVER="org.gjt.mm.mysql.Driver";
 	public static final String DBURL="jdbc:mysql://localhost:3306/jiajiaowang?useUnicode=true&characterEncoding=utf-8";
-	public static final String DBUSER="root";
+	public static final String DBUSER="root";    
 	public static final String DBPASS="btb";
-    
+
 	private Connection conn=null;
 	private PreparedStatement pStat=null;
 	private ResultSet rs=null;
